99b49d44924dec10ce0a37eea3239c97ca3017ca
agilimam <agilimamalhafizi04@gmail.com>
2024-10-18 18:28:19 +0700

    Melanjutkan tugas di kelas

---
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.pertemuan3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.createFontFamilyResolver
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan3.ui.theme.Pertemuan3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pertemuan3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompose(modifier = Modifier.padding(innerPadding) )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose(
+    modifier: Modifier = Modifier
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+
+    ){
+        Text(
+            text = "Indonesia",
+            style = TextStyle(
+                    fontSize = 50.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Red,
+                    )
+
+        )
+        Text(
+            text = "0",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+
+        )
+        Image(painter = painterResource(id = R.drawable.fifa),
+            contentDescription = null)
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pertemuan3Theme {
+        Greeting("Android")
+    }
+}
